cd1fd98b773f5edcbc367d6938dcaac3a59e2483
cash <cash@cashwu.com>
2023-06-08 22:48:41 +0800

    refactor

--- OvertimeHour/OvertimePeriodSettingsTests.cs
+++ OvertimeHour/OvertimePeriodSettingsTests.cs
@@ -12,4 +12,22 @@ public class OvertimePeriodSettingsTests
 
         overtimePeriodSettings.Count.Should().Be(2);
     }
+    
+    [Fact]
+    public void split_overlap_1_period()
+    {
+        var overtimePeriodSettings = new OvertimePeriodSettings(new Period("06:00", "22:00"),
+                                                        new Period("22:00", "06:00"));
+
+        var overTimePeriod = new Period(new DateTime(2023, 10, 08, 18, 00, 00),
+                                        new DateTime(2023, 10, 08, 20, 00, 00));
+
+        var realOvertimePeriods = overtimePeriodSettings.SplitPeriod(overTimePeriod).ToList();
+
+        realOvertimePeriods.Should().BeEquivalentTo(new List<Period>
+        {
+            new("18:00", "20:00"),
+        }, options => options.Excluding(a => a.StartDateTime)
+                             .Excluding(a => a.EndDateTime));
+    }
 }
